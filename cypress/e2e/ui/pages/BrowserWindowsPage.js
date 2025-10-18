1b9a3d201d6c15434b545390bd7320dcbf71ef92
LEVIR CESAR RIBEIRO LEMOS - 215797 <levir.lemos@cagece.com.br>
2025-10-18 13:07:38 -0300

    feat: created browser windows test

---
+++ cypress/e2e/ui/pages/BrowserWindowsPage.js
@@ -0,0 +1,29 @@
+export class BrowserWindowsPage {
+  elements = {
+    formsMenu: () => cy.contains("h5", "Alerts, Frame & Windows"),
+    browserWindowsMenuItem: () => cy.contains("span.text", "Browser Windows"),
+    windowButton: () => cy.get("#windowButton"), 
+    modalTitle: () => cy.get(".modal-title"),
+    modalTableValue: (label) => cy.get("td").contains(label).next(),
+    closeModalButton: () => cy.get("#closeLargeModal"),
+  };
+
+  visit() {
+    cy.visit("https://demoqa.com/");
+  }
+
+  navigateToBrowserWindows() {
+    this.elements.formsMenu().click();
+    this.elements.browserWindowsMenuItem().click();
+  }
+ 
+  clickWindowButton() {
+    this.elements.windowButton().click();
+  }
+
+  
+
+  closeModal() {
+    this.elements.closeModalButton().click({ force: true });
+  }
+}
---
+++ cypress/e2e/ui/tests/BrowserWindows.cy.js
@@ -0,0 +1,38 @@
+import { BrowserWindowsPage } from "../pages/BrowserWindowsPage";
+
+describe("Browser Windows - Padrão Page Object", () => {
+  const browserWindows = new BrowserWindowsPage();
+
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    browserWindows.visit();
+  });
+
+  it("Deve abrir e validar a janela", () => {
+    browserWindows.navigateToBrowserWindows();
+
+    cy.window().then((win) => {
+      cy.stub(win, "open").as("popup"); 
+    });
+
+    browserWindows.clickWindowButton();
+
+    cy.get("@popup").then((stub) => {
+      const relativeUrl = stub.getCall(0).args[0];
+
+      cy.location("origin").then((origin) => {
+        const fullUrl = `${origin}${relativeUrl}`;
+
+        cy.visit(fullUrl);
+      });
+    });
+    cy.contains("This is a sample page").should("be.visible");
+
+    cy.screenshot();
+
+    //Por limitções do cypress não é possível fechar a janela 
+    //pois o cypress não interaje com múltiplas janelas.
+    //Se o teste fosse em selenium isso seria possível usando o switchTo
+  });
+});
